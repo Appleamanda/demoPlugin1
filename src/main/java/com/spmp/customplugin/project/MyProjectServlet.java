21a1cfac8225c90392a7ca38571622cf130959d0
Appleamanda <yz0420@163.com>
2020-02-18 13:24:05 +0800

    create demoPlugin1

---
+++ src/main/java/com/spmp/customplugin/project/MyProjectServlet.java
@@ -0,0 +1,66 @@
+package com.spmp.customplugin.project;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.net.URI;
+import com.atlassian.sal.api.auth.LoginUriProvider;
+import com.atlassian.sal.api.user.UserManager;
+
+import com.atlassian.templaterenderer.TemplateRenderer;
+
+import javax.inject.Inject;
+import com.atlassian.plugin.spring.scanner.annotation.component.Scanned;
+import com.atlassian.plugin.spring.scanner.annotation.imports.ComponentImport;
+
+public class MyProjectServlet extends HttpServlet
+{
+    @ComponentImport
+    private final UserManager userManager;
+    @ComponentImport
+    private final LoginUriProvider loginUriProvider;
+    @ComponentImport
+    private final TemplateRenderer renderer;
+
+    @Inject
+    public MyProjectServlet(UserManager userManager, LoginUriProvider loginUriProvider, TemplateRenderer renderer)
+    {
+        this.userManager = userManager;
+        this.loginUriProvider = loginUriProvider;
+        this.renderer = renderer;
+    }
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException
+    {
+        String username = userManager.getRemoteUsername(request);
+        if (username == null || !userManager.isSystemAdmin(username))
+        {
+            redirectToLogin(request, response);
+            return;
+        }
+
+        response.setContentType("text/html;charset=utf-8");
+        renderer.render("project.vm", response.getWriter());
+    }
+
+    private void redirectToLogin(HttpServletRequest request, HttpServletResponse response) throws IOException
+    {
+        response.sendRedirect(loginUriProvider.getLoginUri(getUri(request)).toASCIIString());
+    }
+
+    private URI getUri(HttpServletRequest request)
+    {
+        StringBuffer builder = request.getRequestURL();
+        if (request.getQueryString() != null)
+        {
+            builder.append("?");
+            builder.append(request.getQueryString());
+        }
+        return URI.create(builder.toString());
+    }
+}
